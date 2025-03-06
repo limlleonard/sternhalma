196924b1d7854ddad0f3a061a895849ed501a609
limlleonard <limlleonard@gmail.com>
2025-03-06 09:40:55 +0100

    trivial

--- game/models.py
+++ game/models.py
@@ -5,3 +5,10 @@ class Score(models.Model):
     name=models.CharField(max_length=20)
     def __str__(self):
         return f'{self.name}: {self.score}'
+
+class Board(models.Model):
+    int1=models.IntegerField()
+    float1=models.FloatField()
+
+class Piece(models.Model):
+    pass
