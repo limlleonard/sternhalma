90e3be02c0099b5d9cc9d8cda6759425039fd78b
limlleonard <limlleonard@gmail.com>
2025-03-06 16:43:15 +0100

    build2

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
 cd front
 npm install
 npm run build
+cd ..
+mkdir -p templates
+cp front/dist/index.html templates/index.html
+python manage.py collectstatic --no-input
+python manage.py migrate
